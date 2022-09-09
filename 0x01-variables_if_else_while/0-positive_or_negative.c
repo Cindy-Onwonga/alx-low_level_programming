e9dd5c9c7560b2486d450b9ed5ddd14e9a3201cd
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-09 14:29:48 -0700

    betty check

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,9 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if ( n<0 )
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("invalid number");
-	
 	return (0);
 }
