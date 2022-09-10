34989c3454bdbb21f70d8be89f907f042244cf7e
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-10 01:14:41 -0700

    print single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig < 10; dig++)
+		printf("%d",dig);
+
+	putchar('\n');
+	return (0);
+}
