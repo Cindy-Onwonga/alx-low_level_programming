56196361d1440fb913ceaa30e056738469afed61
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-10 03:30:31 -0700

    all cmbinations of single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig < 10; dig++)
+	{
+		putchar ((dig % 10) + '0');
+		if (dig == 9)
+			continue;
+
+		putchar (',');
+		putchar (' ');
+	}
+	return (0);
+}
