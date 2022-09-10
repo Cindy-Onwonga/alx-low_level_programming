6af14d5f85619d1df264a9e669aea489ec24585d
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-10 02:31:33 -0700

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dig;
+
+	for (dig = 48; dig < 48; dig++)
+		putchar(dig);
+
+	putchar('\n');
+	return (0);
+}
