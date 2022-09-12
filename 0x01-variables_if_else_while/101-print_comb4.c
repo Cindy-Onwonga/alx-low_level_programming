859f9fc101a464aee928b872675301d8d6cd3a6e
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-12 10:10:35 -0700

    combinations of three

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	a = 0;
+	while (a < 1000)
+	{
+		b = a / 100;
+		c = (a / 10) % 10;
+		d = a % 10;
+		if (b < c && c < d)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+			putchar(d + '0');
+			if (a < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
