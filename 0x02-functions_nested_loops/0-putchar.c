0b150e5e71aadedea341e65d4516308b26261443
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-15 03:00:15 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * putChar - prints putchar followed by new linw
+ *
+ * Return: void
+ */
+void putChar(void)
+{
+	int chars;
+
+	chars = '_';
+	_putchar(chars);
+
+	chars = 'p';
+	_putchar(chars);
+
+	chars = 'u';
+	_putchar(chars);
+
+	chars = 't';
+	_putchar(chars);
+
+	chars = 'c';
+	_putchar(chars);
+
+	chars = 'h';
+	_putchar(chars);
+
+	chars = 'a';
+	_putchar(chars);
+
+	chars = 'r';
+	_putchar(chars);
+
+	chars = '\n';
+	_putchar(chars);
+
+	return;
+}
