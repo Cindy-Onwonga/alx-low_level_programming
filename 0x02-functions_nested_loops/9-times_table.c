228c96a021e0c43c165f15a4a3341cf7ea846328
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-15 18:13:06 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,9 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			prod = i * j;
-			if 
+			_putchar(((prod / 10) % 10) + '0');
+			_putchar((prod % 10) + '0');
+
 			_putchar(',');
 		}
 		_putchar('\n');
