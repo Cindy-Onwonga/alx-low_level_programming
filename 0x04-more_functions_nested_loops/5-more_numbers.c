55895ffc855f9c46a0d391bcd932ea712e499321
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-17 04:57:27 -0700

    print straight line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <=14; i++)
+	for (i = 0; i <= 14; i++)
 	{
 		for (j = 0; j < 9; j++)
 			_putchar(i + '0');
 
-		_putchar('\n');
+		
 	}
-	return;
+	_putchar('\n');
+	
 }
