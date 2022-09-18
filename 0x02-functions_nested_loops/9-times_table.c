52a68fd61f5b1c1fd50f7b46d7f05a1dbf54955d
Cindy-Onwonga <cindykerubo@gmail.com>
2022-09-18 01:24:45 -0700

    debugging

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,16 +8,13 @@
 
 void times_table(void)
 {
-	int i, j, prod;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			prod = i * j;
-			_putchar(((prod / 10) % 10) + '0');
-			_putchar((prod % 10) + '0');
-
+			_putchar((i * j) + '0');
 			_putchar(',');
 		}
 		_putchar('\n');
