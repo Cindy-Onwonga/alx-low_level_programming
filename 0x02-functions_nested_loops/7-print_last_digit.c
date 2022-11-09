cd07d4d095dd6ba554f1dfe06787eed250e005c3
Cindy-Onwonga <cindykerubo@gmail.com>
2022-11-09 03:54:25 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,15 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		if (n = INT_MIN)
-			int ld = -( n % 10);
+	int ld;
 
+	if (n < 0)
 		n = -n;
-	}
-	int ld = n % 10;
+
+	ld = n % 10;
+
+	if (ld < 0)
+		ld = -ld;
 
 	_putchar(ld + '0');
 	return (ld);
