2f77ddb308dfe82d2591c6804382f6c57e416b96
Cindy-Onwonga <cindykerubo@gmail.com>
2022-11-10 06:23:33 -0800

    sum of 3 & 5 multiples less that 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,12 @@ int main(void)
 {
 	int a, sum;
 
-	for ( a = 0; a < 1024; a++)
+	if (a % 3 == 0 || a % 5 == 0)
 	{
-		if (a % 3 == 0 || a % 5 == 0)
-		{
+		for (a = 0; a < 1024; a++)
 			sum += a;
-			printf("%d\n", sum);
-		}
+
 	}
+	printf("%d\n", sum);
 	return (0);
 }
