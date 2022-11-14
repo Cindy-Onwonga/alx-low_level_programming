d12c76559a5766bbd84d08c0b47d74e4319691b0
Cindy-Onwonga <cindykerubo@gmail.com>
2022-11-14 01:09:24 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j < 9; j++)
-			_putchar(i + '0');
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');
 
-		
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	
 }
