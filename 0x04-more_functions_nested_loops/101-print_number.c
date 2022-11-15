efe5147794776ccd9b0af3f301830db5f0dfcf14
Cindy-Onwonga <cindykerubo@gmail.com>
2022-11-15 03:49:31 -0800

    print func numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_number - print number passed in function
@@ -9,5 +10,18 @@
  */
 
 void print_number(int n)
-{	
+{
+	unsigned int c;
+
+	if (n < 0)
+	{
+		c = -n;
+		_putchar('-');
+	} else 
+		c = n;
+
+	if (c / 10)
+		print_number(c / 10);
+
+	_putchar((c % 10) + '0');
 }
