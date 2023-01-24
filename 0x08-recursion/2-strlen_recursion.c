54b1946332082763ddab20fbb3fc7dffbea49ede
Cindy-Onwonga <cindykerubo@gmail.com>
2023-01-24 13:15:03 -0800

    string length but recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return string length
+ * @s: the string
+ *
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recusion(s + 1));
+}
