ba002974432adf860b2b7884cfaabdf80e3cf34b
Cindy-Onwonga <cindykerubo@gmail.com>
2023-01-25 09:12:11 -0800

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print product of 2 numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num, digit;
+	int sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num])
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
