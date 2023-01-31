dd82f8f9fda291c2277e7a402778a84d8d1804a5
Cindy-Onwonga <cindykerubo@gmail.com>
2023-01-31 10:59:05 -0800

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,14 +8,14 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i = 0;
 	char *str, *sep = "";
 
 	va_list list;
 
 	va_start(list, format);
 
-	if (fomrat)
+	if (format)
 	{
 		while (format[i])
 		{
