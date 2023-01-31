977c6f29a58b97c44c44a2fef6a34ef836c63633
Cindy-Onwonga <cindykerubo@gmail.com>
2023-01-31 00:42:41 -0800

    print sentence before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - print sentence before main
+ *
+ * Return: nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
