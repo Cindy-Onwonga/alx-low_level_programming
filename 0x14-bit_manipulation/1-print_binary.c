713a707852e5e2abc950605c771632b29d405cd9
Cindy-Onwonga <cindykerubo@gmail.com>
2023-02-07 10:45:59 -0800

    print binary value of decimal

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print binary code of number
+ * @n: decimal integer
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned int n)
+{
+	unsigned long int this;
+	int i;
+	into count = 0;
+
+	for (i = 63; i >= 0; i--)
+	{
+		this = n >> i;
+
+		if (this & 1)
+		{
+			putchar('1');
+			count++;
+		}
+		else if (count)
+			putchar('0');
+	}
+
+	if (!count)
+		putchar('0');
+}
