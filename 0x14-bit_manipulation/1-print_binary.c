600550acec932b28d91212547c4cf0f1f31425fb
Cindy-Onwonga <cindykerubo@gmail.com>
2023-02-07 10:52:00 -0800

    print binary value of decimal

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,11 +7,11 @@
  *
  * Return: nothing
  */
-void print_binary(unsigned int n)
+void print_binary(unsigned long int n)
 {
 	unsigned long int this;
 	int i;
-	into count = 0;
+	int count = 0;
 
 	for (i = 63; i >= 0; i--)
 	{
