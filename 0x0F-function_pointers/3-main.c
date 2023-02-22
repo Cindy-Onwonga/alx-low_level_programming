da8f8b546a315e64f807f221064592e8ac8d75f8
Cindy-Onwonga <cindykerubo@gmail.com>
2023-02-22 03:49:21 -0800

    simple calculations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,32 +11,32 @@
   */
 int main(int argc, char *argv[])
 {
-	int a = 0, b = 0, result = 0;
-	char s;
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
+	if (
+			(strcmp(argv[2], "+") &&
+			 strcmp(argv[2], "-") &&
+			 strcmp(argv[2], "*") &&
+			 strcmp(argv[2], "/") &&
+			 strcmp(argv[2], "%"))
+		)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	s = argv[2][0];
-
-	if (s != '+' && s != '-' && s != '*' && s != '/' && s != '%')
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+			&& atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	result = (get_op_func(argv[2]))(a, b);
-	printf("%d\n", result);
+	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+
 	return (0);
 }
