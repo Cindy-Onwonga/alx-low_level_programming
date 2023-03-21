2ca1958894bfff8602a1e41eb25ae811dfe3e840
Cindy-Onwonga <cindykerubo@gmail.com>
2023-03-21 10:05:51 -0700

    insert node at index n

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,6 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->next = temp->next;
 			new->prev = temp;
 			temp->next->prev = new;
+			temp->next = new;
 			return (new);
 		}
 		else
